1ee5e4c0126f4c904a8251a85d85877b4d522eec
Dastan <dastan@MacBook-Air-Dastan.local>
2023-05-15 13:41:55 +0600

    rich

--- fire.js
+++ fire.js
@@ -7,13 +7,13 @@ import "firebase/compat/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA4p3C1yJa0fYRxYwvlU8t1EyLvFwcn0nY",
-  authDomain: "riche-fb30c.firebaseapp.com",
-  projectId: "riche-fb30c",
-  storageBucket: "riche-fb30c.appspot.com",
-  messagingSenderId: "282562355554",
-  appId: "1:282562355554:web:7bd0437d09041604298c3b"
-};
+    apiKey: "AIzaSyA4p3C1yJa0fYRxYwvlU8t1EyLvFwcn0nY",
+    authDomain: "riche-fb30c.firebaseapp.com",
+    projectId: "riche-fb30c",
+    storageBucket: "riche-fb30c.appspot.com",
+    messagingSenderId: "282562355554",
+    appId: "1:282562355554:web:7bd0437d09041604298c3b"
+  };
 
 // Initialize Firebase
 const fire = firebase.initializeApp(firebaseConfig);
